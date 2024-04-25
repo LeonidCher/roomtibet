3e0de7fd39a4ccd3f70fc7142468e850d677f4a4
LeonidCher <4ls2005@mail.ru>
2024-04-26 01:39:32 +0400

    адаптив без модалки

--- roomtibet/script.js
+++ roomtibet/script.js
@@ -1,13 +1,21 @@
 const arrowsDown = document.querySelectorAll('.info__icon')
 const selectBlocks = document.querySelectorAll('.info__selectCategory')
-arrowsDown.forEach((el) => {
-    el.addEventListener('click', (el)=>{
-        console.log(el)
-        selectBlocks.forEach((el)=>{
-            el.classList.toggle('info__selectCategory_active')
-        })
-    })
+
+
+arrowsDown[0].addEventListener('click', (el)=>{
+    selectBlocks[0].classList.toggle('info__selectCategory_active')
 })
+arrowsDown[1].addEventListener('click', (el) => {
+    selectBlocks[1].classList.toggle('info__selectCategory_active')
+})
+// arrowsDown.forEach((el) => {
+//     el.addEventListener('click', (el)=>{
+//         console.log(el)
+//         selectBlocks.forEach((el)=>{
+//             el.classList.toggle('info__selectCategory_active')
+//         })
+//     })
+// })
 // const arrowsDown = document.querySelectorAll('.info__icon')
 // const selectBlocks = document.querySelectorAll('.info__selectCategory')
 // for (let i = 0; i < arrowsDown.length; i++) {
